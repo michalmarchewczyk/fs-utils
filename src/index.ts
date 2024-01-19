b61fedd3c92b49ae8e3a4d5052e0dbd72b16ffbc
Michał Marchewczyk <dev@marchewczyk.eu>
2024-01-20 00:46:42 +0100

    Setup Sass and Bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { engine } from "express-handlebars";
+import path from "node:path";
 
 const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', 'src/views');
+
+app.use('/css', express.static(path.join(__dirname, './css/')))
 
 app.get('/', (req, res) => {
   res.render('home');
